2bfe16ac3a047b953f490446bf45ea42b7974042
ayankundu77 <ayankundu055@gmail.com>
2026-07-24 02:08:38 +0530

    Time: 91 ms (11.36%), Space: 55.4 MB (22.3%) - LeetHub

---
+++ 0472-concatenated-words/0472-concatenated-words.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isConcatenated(String word, Set<String>st, Map<String,Boolean> hm){
+        if(hm.containsKey(word)){
+            return hm.get(word);
+        }
+        for(int i=0;i<word.length();i++){
+            String prefix = word.substring(0,i+1);
+            String suffix = word.substring(i+1,word.length());
+            if(st.contains(prefix) && st.contains(suffix) || st.contains(prefix) && isConcatenated(suffix,st,hm)){
+                hm.put(word,true);
+                return true;
+            }
+        }
+        hm.put(word,false);
+            return false;
+    }
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        HashMap<String, Boolean> hm = new HashMap<>();
+        Set<String> hs = new HashSet<>();
+        for(String word:words){
+            hs.add(word);
+        }
+        List result = new ArrayList<>();
+        for(int i=0;i<words.length;i++){
+            if(isConcatenated(words[i], hs, hm)){
+                result.add(words[i]);
+            }
+        }
+        return result;
+    }
+}
