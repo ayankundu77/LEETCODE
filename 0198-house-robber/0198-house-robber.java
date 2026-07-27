3bf206fc1ee335664440dd777228fd9da1701620
ayankundu77 <ayankundu055@gmail.com>
2026-07-28 01:06:08 +0530

    Time: 0 ms (100%), Space: 42.7 MB (57.31%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,15 +1,16 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,0);
-        dp[0]=nums[0];
+        int prev=nums[0];
+        int prev2 = 0;
         for(int i=1;i<n;i++){
             int pick = nums[i];
-            if(i>1) pick+=dp[i-2];
-            int notPick = 0 + dp[i-1];
-            dp[i] = Math.max(pick,notPick);
+            if(i>1) pick+=prev2;
+            int notPick = 0 + prev;
+            int curr = Math.max(pick,notPick);
+            prev2=prev;
+            prev=curr;
         }
-        return dp[n-1];
+        return prev;
     }
 }
