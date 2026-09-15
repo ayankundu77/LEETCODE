bb7e29519ec316816d82ed417b2632857f114ad0
ayankundu77 <ayankundu055@gmail.com>
2026-09-16 00:41:16 +0530

    Time: 2189 ms (5.03%), Space: 183.2 MB (9.55%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -8,31 +8,25 @@ class Solution {
         }
         return true;
     }
-    public int maxPalin(String s, int k, int i, int j, int[][] dp){
-        if(i>=n || j>=n) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        if(isPalindrome(s,i,j)){
-            int take = 1+maxPalin(s,k,j+1,j+k,dp);
-            int grow = maxPalin(s,k,i,j+1,dp);
-            int slide = maxPalin(s,k,i+1,j+1,dp);
-
-            return dp[i][j] = Math.max(Math.max(take,grow),slide);
-        }
-            int grow = maxPalin(s,k,i,j+1,dp);
-            int slide = maxPalin(s,k,i+1,j+1,dp);
-
-            return dp[i][j] = Math.max(grow,slide);
-    }
     public int maxPalindromes(String s, int k) {
         n = s.length();
         if(k==1) return n;
         int[][] dp = new int[n+1][n+1];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                dp[i][j]=-1;
+        for(int i=n-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(isPalindrome(s,i,j)){
+                    int take = 1+(j+k<=n ? dp[j+1][j+k] : 0);
+                    int grow = dp[i][j+1];
+                    int slide = dp[i+1][j+1];
+
+                    dp[i][j] = Math.max(Math.max(take,grow),slide);
+                }
+                int grow = dp[i][j+1];
+                int slide = dp[i+1][j+1];
+
+                dp[i][j] = Math.max(Math.max(dp[i][j],grow),slide);
             }
         }
-        return maxPalin(s,k,0,k-1,dp);
+        return dp[0][k-1];
     }
 }
