7455cdabdcca65c1568cb6eca38e28044772a7c7
ayankundu77 <ayankundu055@gmail.com>
2026-09-26 19:30:12 +0530

    Time: 5 ms (66.47%), Space: 46.9 MB (82.38%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,20 +1,37 @@
 class Solution {
-    public long func(int i, int j, String s, String t, long[][] dp){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s.charAt(i)==t.charAt(j)) return dp[i][j]=func(i-1,j-1,s,t,dp);
-        return dp[i][j]=1+Math.min(func(i-1,j,s,t,dp),Math.min(func(i,j-1,s,t,dp),func(i-1,j-1,s,t,dp)));
-    }
     public int minDistance(String word1, String word2) {
+
         int n = word1.length();
         int m = word2.length();
-        long[][] dp = new long[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                dp[i][j]=-1;
+
+        long[][] dp = new long[n + 1][m + 1];
+
+        for(int j = 0; j <= m; j++)
+            dp[0][j] = j;
+
+        for(int i = 0; i <= n; i++)
+            dp[i][0] = i;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+
+                if(word1.charAt(i - 1) == word2.charAt(j - 1)) {
+
+                    dp[i][j] = dp[i - 1][j - 1];
+
+                } else {
+
+                    dp[i][j] = 1 + Math.min(
+                        dp[i - 1][j],
+                        Math.min(
+                            dp[i][j - 1],
+                            dp[i - 1][j - 1]
+                        )
+                    );
+                }
             }
         }
-        return (int)func(n-1,m-1,word1,word2,dp);
+
+        return (int)dp[n][m];
     }
 }
